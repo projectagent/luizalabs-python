aaac8322a5b8c9cfb6ef0e2c13d9c44fcfec1afb
projectagent <oliveirarossinia@gmail.com>
2026-09-22 15:55:13 -0300

    Operadores de Associação

---
+++ Primeiros Passos e Versionamento de Código/Operadores de Associação/operadores_associação.py
@@ -0,0 +1,11 @@
+# Operadores utilizados para verificar se um objeto está presente em uma sequência
+
+curso = "Curso de Python"
+frutas = ["laranja", "uva", "limão"]
+saques = [1500, 00]
+
+print("Pyhton" in curso)
+
+print("Maçã" not in  frutas)
+
+print(200 in saques)
