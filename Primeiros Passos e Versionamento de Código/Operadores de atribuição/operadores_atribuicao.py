99c4b7707b2d44d6a24e28ea4f00c85b3fa71df2
projectagent <oliveirarossinia@gmail.com>
2026-09-22 15:01:27 -0300

    Operadores de Atribuição

---
+++ Primeiros Passos e Versionamento de Código/Operadores de atribuição/operadores_atribuicao.py
@@ -0,0 +1,30 @@
+# Atribuição Simples
+saldo = 450
+print(saldo)
+
+# Atribuição com Adição
+saldo += 500
+print(saldo)
+
+# Atribuição com Subtração
+saldo -= 200
+print(saldo)
+
+# Atribuição com Multiplicação
+saldo *= 2
+print(saldo)
+
+# Atribuição com Divisão
+saldo //= 4
+print(saldo)
+
+saldo /= 2
+print(saldo)
+
+# Atribuição com Módulo
+saldo %= 3
+print(saldo)
+
+# Atribuição com Exponenciação
+saldo **= 10
+print(saldo)
